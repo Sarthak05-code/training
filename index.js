492849af1918ab6eacee9a78f4018dbabd983381
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-10 09:21:38 +0545

    array and object

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 const obj = {
-    name : "Sarthak",
-    age : 20 , 
-    class : "BCA"
-}
+  name: "Sarthak",
+  age: 20,
+  class: "BCA",
+};
 
 console.log(obj);
 
 const objArray = {
-     array : [1,2,3,4,5],
-     returnIndex : function(i) {
-        return objArray.array[i]
-     }
-     
-}
-objArray.array.push(6)
-
+  array: [1, 2, 3, 4, 5],
+  returnIndex: function (i) {
+    return objArray.array[i];
+  },
+};
+objArray.array.push(6);
 
 console.log(objArray.array);
 
-objArray.array.pop()
+objArray.array.pop();
 
 console.log(objArray.array);
 
 console.log("The index is : ", objArray.returnIndex(3));
 
-
 const objNew = {
-    arrayNew = [
-        objInner = {
-            arrayAgain : [100,200,300,400,500]
-        }
-    ]
-}
-console.log(objNew.arrayNew);
-
+  arrayNew: [{ name: "Sarthak", age: 20, class: "BCA" },
+            {gender : "Male" , classes : ["JavaScript", "DBMS", "SE","Numerical","OS"]}
+  ],
+};
+console.log(objNew.arrayNew[1].classes[4]);
 
 
 
